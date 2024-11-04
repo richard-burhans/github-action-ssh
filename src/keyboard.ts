8596c194599be578bee43842851c03d132776bfd
Richard Burhans <richard.burhans@gmail.com>
2024-11-04 18:36:47 -0500

    updating to node 22.11.0

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -1,14 +1,18 @@
-export const keyboardFunction = password => (
-  name,
-  instructions,
-  instructionsLang,
-  prompts,
-  finish
-) => {
-  if (
-    prompts.length > 0 &&
-    prompts[0].prompt.toLowerCase().includes("password")
-  ) {
-    finish([password]);
-  }
-};
+import {Prompt, KeyboardInteractiveCallback} from 'ssh2';
+
+export const keyboardFunction =
+  (password: string) =>
+  (
+    name: string,
+    instructions: string,
+    lang: string,
+    prompts: Prompt[],
+    finish: KeyboardInteractiveCallback
+  ) => {
+    if (
+      prompts.length > 0 &&
+      prompts[0].prompt.toLowerCase().includes('password')
+    ) {
+      finish([password]);
+    }
+  };
